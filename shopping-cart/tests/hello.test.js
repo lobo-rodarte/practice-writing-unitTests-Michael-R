0f76f664ca7e31d34223195cc2f1c5625ab16973
mrodarte <mlrfortune@gmail.com>
2025-09-11 12:44:10 -0500

    updated the code

--- shopping-cart/tests/hello.test.js
+++ shopping-cart/tests/hello.test.js
@@ -5,11 +5,16 @@ const getTotalItems = require('../cart.js');
 const item = 'apple';
 const cart = 'basket';
 const quantity = 1;
+const invalidQuantity = -1
 
 describe(item + ' was added to the ' + cart + ' with a quantity of ' + quantity, function() {
     test('should return a message showing the item added',
 function() {
-    expect(shoppingCart.addItem('apple', 'basket', 1)).toBe('apple was added to the basket with a quantity of 1')
+    expect(shoppingCart.addItem(item, cart, quantity)).toBe('apple was added to the basket with a quantity of 1')
+});
+    test('should return an invalid number',
+function() {
+    expect(shoppingCart.addItem(item, cart, invalidQuantity)).toBe('apple was added to the basket with a quantity of -1')
 });
 
 });
